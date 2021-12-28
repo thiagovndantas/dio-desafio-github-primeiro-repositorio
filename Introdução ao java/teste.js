6f2c5c5a9914f1dd44fa71eeb4c5f8d4da36f7d7
thiagovndantas <thiagovndantas@gmail.com>
2021-12-28 17:03:11 -0300

    subindo teste

--- Introdução ao java/teste.js
+++ Introdução ao java/teste.js
@@ -1,15 +1,3 @@
-function returnEvenValues(array){
-    let evenNums = [];
-    for(let i = 0; i < array.length; i++){
-        if(array[i] % 2 === 0 ) {
-            evenNums.push(array[i]);
-        } else {
-            console.log(`${array[i]} não é par!`)
-        }
-    }
-        console.log('Os números pares são: ',evenNums)
+function soma(a,b){
+    return a+b
 }
-
-let array = [1,2,3,4,5,7,8];
-
-returnEvenValues(array);
