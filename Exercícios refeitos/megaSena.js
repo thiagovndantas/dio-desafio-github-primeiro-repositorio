b3510c9846e39200ddf9888a5a12e76ecf766ef9
thiagovndantas <thiagovndantas@gmail.com>
2021-12-31 10:28:15 -0300

    Sorteio mega

---
+++ Exercícios refeitos/megaSena.js
@@ -0,0 +1,17 @@
+let numerosEscolhidos = [14, 05, 13, 31, 42];
+let numerosNovos = [];
+
+function getRandomInt(min, max) {
+    for(let i = 0; i < 2;) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        random = Math.floor(Math.random() * (max - min)) + min;
+        if (!numerosNovos.includes(random)) {
+            numerosNovos.unshift(random);
+            i++
+        }
+    }
+    console.log(numerosNovos)
+}
+
+getRandomInt(0,60);
