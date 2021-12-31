91dabecaea8fe649843a89ce5764666cf2ade467
thiagovndantas <thiagovndantas@gmail.com>
2021-12-31 19:51:53 -0300

    Atualização do teste

---
+++ Exercícios refeitos/objetos.js
@@ -0,0 +1,8 @@
+let Thiago = {
+    idade: 24,
+    formacao: {curso: 'Engenharia Eletrica', universidade: 'UPE', conclusao: 'set/21'},
+    profissao: {profissao: 'Analista Financeiro', empresa: 'Grupo Cornélio Brennand'},
+    objetivo: 'Realizar transição de área e iniciar a carreira como desenvolvedor',
+    competencias: ['pacote office','SAP','python', 'javascript'],
+    qualidades: ['proativo','versátil','determinado']
+}
